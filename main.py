59e8e12a4b1b615d2cd404f8e906937ca4ce8aa3
Altamar <maga99911@mail.ru>
2024-03-27 22:45:21 +0300

    git commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import requests
 from random import randint
 
+from googletrans import Translator
+import requests
 import telebot
 from sqlalchemy import text
-from googletrans import Translator
 
-from db_connect import db
 from config import TELEGRAM_TOKEN, TRAINING_URL, CALORIE_URL, RECIPE_URL, TRAINING_API, RECIPE_API, CALORIE_API
+from db_connect import db
 
 translator = Translator()
 
@@ -19,13 +19,13 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     start_msg = (
-        f"Приветствую. У данного бота есть несколько функций:"
-        f"\n/calorie - Позволяет получить калории потребленного продукта на 100г."
-        f"\n/recipe - Позволяет получить рецепты и способы приготовления."
-        f"\n/training - Позволяет получить советы по тренировкам и разным активностям."
-        f"\n/products - Позволяет получить список потребленных продуктов."
-        f"\n/total - Позволяет получить общее количество потребленных калорий."
-        f"\n/delete - Позволяет удалить данные потребленных продуктов."
+        f"Приветствую. У данного бота есть несколько функций:\n"
+        f"/calorie - Позволяет получить калории потребленного продукта на 100г.\n"
+        f"/recipe - Позволяет получить рецепты и способы приготовления.\n"
+        f"/training - Позволяет получить советы по тренировкам и разным активностям.\n"
+        f"/products - Позволяет получить список потребленных продуктов.\n"
+        f"/total - Позволяет получить общее количество потребленных калорий.\n"
+        f"/delete - Позволяет удалить данные потребленных продуктов."
     )
 
     bot.send_message(
@@ -55,56 +55,83 @@ def training_com(message):
 @bot.message_handler(commands=['training'])
 def get_training(message):
     try:
-        training = message.text
-        querystring = {"intensitylevel": {training}}
+        training = int(message.text)
+    except ValueError:
+        bot.reply_to(
+            message, (f'Ошибка. Вы ввели не число.'))
 
+    querystring = {"intensitylevel": {training}}
+
+    try:
         response = requests.get(
             TRAINING_URL, headers=TRAINING_API, params=querystring)
-        if response.status_code == 200:
-            data = response.json()
-        number_train = randint(1, 10)
+    except requests.ConnectionError as e:
+        bot.reply_to(
+            message, (f'Ошибка {e}! Невозможно связаться с сервером.'))
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        bot.reply_to(
+            message, (f'Ошибка: {response.status_code}'))
+
+    number_train = randint(1, 10)
+
+    try:
         activity = data["data"][number_train]["activity"]
-        metValue = data["data"][number_train]["metValue"]
+        met_value = data["data"][number_train]["metValue"]
         description = data["data"][number_train]["description"]
-        intensityLevel = data["data"][number_train]["intensityLevel"]
+        intensity_level = data["data"][number_train]["intensityLevel"]
         activity_ru = translator.translate(
             activity, src=src, dest=dest).text
         description_ru = translator.translate(
             description, src=src, dest=dest).text
         rate = (
-            f"<b>Название упражнения: </b> {activity_ru}"
-            f"\n<b>Время выполнения (в минутах): </b> {metValue}"
-            f"\n<b>Описание: </b> {description_ru}"
-            f"\n<b>Уровень интенсивности: </b> {intensityLevel}"
+            f"<b>Название упражнения: </b> {activity_ru}\n"
+            f"<b>Время выполнения (в минутах): </b> {met_value}\n"
+            f"<b>Описание: </b> {description_ru}\n"
+            f"<b>Уровень интенсивности: </b> {intensity_level}"
         )
         bot.reply_to(message, rate, parse_mode="HTML")
-    except (KeyError, UnboundLocalError):
+    except (KeyError, UnboundLocalError, IndexError):
         bot.reply_to(
-            message, 'Ошибка! Введите уровень от 1 до 9.')
+            message, (f"Ошибка. Введите уровень от 1 до 9."))
 
 
 @bot.message_handler(commands=['calorie'])
 def get_calorie(message):
     user_id = message.from_user.id
-    try:
-        calorie = message.text
-        calorie_en = translator.translate(calorie, src="ru", dest="en").text
-        querystring = {"query": {calorie_en}, "pageSize": "1",
-                       "pageNumber": "1", "brandOwner": "Kar Nut Products Company"}
+    calorie = message.text
+    calorie_en = translator.translate(calorie, src="ru", dest="en").text
+    querystring = {"query": {calorie_en}, "pageSize": "1",
+                   "pageNumber": "1", "brandOwner": "Kar Nut Products Company"}
 
+    try:
         response = requests.get(
             CALORIE_URL, headers=CALORIE_API, params=querystring)
-        if response.status_code == 200:
-            data = response.json()
+    except requests.ConnectionError as e:
+        bot.reply_to(
+            message, (f"Ошибка {e}! Невозможно связаться с сервером."))
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        bot.reply_to(
+            message, (f"Ошибка: {response.status_code}"))
+
+    try:
         foods_dict = data["foods"][0]["foodNutrients"]
         squirrels = foods_dict[0]["value"]
         fats = foods_dict[1]["value"]
         carbohyd = foods_dict[2]["value"]
         calories_result = float(((squirrels + carbohyd) * 4) + (fats * 9))
-
+    except (KeyError, UnboundLocalError, IndexError):
+        bot.reply_to(
+            message, (f"Возможно вы допустили ошибку в названии, попробуйте снова."))
+    try:
         db.execute(
             text(
-                f'INSERT INTO products_list (user_id, name, calories)'
+                f"INSERT INTO products_list (user_id, name, calories)"
                 f"VALUES ('{user_id}', '{calorie}', '{calories_result}');"
             )
         )
@@ -115,21 +142,26 @@ def get_calorie(message):
             f"{str(calories_result)}"
         )
         bot.reply_to(message, rate, parse_mode="HTML")
-    except (IndexError, UnboundLocalError):
+    except Exception as e:
         bot.reply_to(
-            message, 'Возможно вы ввели что-то неправильно, попробуйте снова.')
+            message, (f"Ошибка {e}."))
 
 
 @bot.message_handler(commands=['products'])
 def get_products(message):
     user_id = message.from_user.id
-    user_products = db.execute(
-        text(
-            f"SELECT * "
-            f"FROM products_list "
-            f"WHERE user_id = {user_id};"
-        )
-    ).fetchall()
+
+    try:
+        user_products = db.execute(
+            text(
+                f"SELECT * "
+                f"FROM products_list "
+                f"WHERE user_id = {user_id};"
+            )
+        ).fetchall()
+    except Exception as e:
+        bot.reply_to(
+            message, (f"Ошибка {e}."))
 
     if user_products:
         rate = "<b>Потребленные продукты: </b>\n"
@@ -144,15 +176,8 @@ def get_products(message):
 def get_calories_total(message):
     user_id = message.from_user.id
     total_calories = 0
-    user_products = db.execute(
-        text(
-            f"SELECT * "
-            f"FROM products_list "
-            f"WHERE user_id = {user_id};"
-        )
-    ).fetchall()
 
-    if user_products:
+    try:
         user_products = db.execute(
             text(
                 f"SELECT * "
@@ -160,6 +185,11 @@ def get_calories_total(message):
                 f"WHERE user_id = {user_id};"
             )
         ).fetchall()
+    except Exception as e:
+        bot.reply_to(
+            message, (f"Ошибка {e}."))
+
+    if user_products:
         for product in user_products:
             total_calories += sum({product.calories})
         rate = f"<b>Общее количество калорий:</b> {total_calories}"
@@ -171,13 +201,19 @@ def get_calories_total(message):
 @bot.message_handler(commands=['delete'])
 def delete_products(message):
     user_id = message.from_user.id
-    user_products = db.execute(
-        text(
-            f"SELECT COUNT(*) "
-            f"FROM products_list "
-            f"WHERE user_id = {user_id};"
+    
+    try:
+        user_products = db.execute(
+            text(
+                f"SELECT COUNT(*) "
+                f"FROM products_list "
+                f"WHERE user_id = {user_id};"
+            )
         )
-    )
+    except Exception as e:
+        bot.reply_to(
+            message, (f"Ошибка {e}."))
+
     count = user_products.fetchone()[0]
 
     if count == 0:
@@ -197,16 +233,25 @@ def delete_products(message):
 
 @bot.message_handler(commands=['recipe'])
 def get_recipe(message):
-    try:
-        name_dish = message.text
-        name_dish_en = translator.translate(
-            name_dish, src="ru", dest="en").text
-        querystring = {"query": {name_dish_en}}
+    name_dish = message.text
+    name_dish_en = translator.translate(
+        name_dish, src="ru", dest="en").text
+    querystring = {"query": {name_dish_en}}
 
+    try:
         response = requests.get(
             RECIPE_URL, headers=RECIPE_API, params=querystring)
-        if response.status_code == 200:
-            data = response.json()
+    except requests.ConnectionError as e:
+        bot.reply_to(
+            message, (f"Ошибка {e}! Невозможно связаться с сервером."))
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        bot.reply_to(
+            message, (f"Ошибка: {response.status_code}"))
+
+    try:
         title = data[0]["title"]
         ingredients = data[0]["ingredients"]
         instructions = data[0]["instructions"]
@@ -216,14 +261,14 @@ def get_recipe(message):
         instructions_ru = translator.translate(
             instructions, src=src, dest=dest).text
         rate = (
-            f"<b>Название блюда:</b> " + title_ru + "\n"
-            f"<b>Ингредиенты:</b> " + ingredients_ru + "\n"
-            f"<b>Способ приготовления:</b> " + instructions_ru
+            f"<b>Название блюда:</b> {title_ru}\n"
+            f"<b>Ингредиенты:</b> {ingredients_ru}\n"
+            f"<b>Способ приготовления:</b> {instructions_ru}"
         )
         bot.reply_to(message, rate, parse_mode="HTML")
-    except IndexError:
+    except (KeyError, UnboundLocalError, IndexError):
         bot.reply_to(
-            message, 'Возможно вы ввели что-то неправильно, попробуйте снова.')
+            message, "Возможно вы ввели что-то неправильно, попробуйте снова.")
 
 
 def main():
